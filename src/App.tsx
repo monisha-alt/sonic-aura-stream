d60c9eff0433d1c4f1bd4a79c9696990887c0d2a
Monisha <mdr2825color@gmail.com>
2025-10-23 22:02:03 +0530

    Fix: Update App.tsx routes and gitignore for landing page deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
 import Index from './pages/Index'
-import EmotionRecommendations from './pages/EmotionRecommendations'
-import Trending from './pages/Trending'
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<LandingPage />} />
       <Route path="/landing" element={<LandingPage />} />
-      <Route path="/" element={<Index />} />
-      <Route path="/emotion" element={<EmotionRecommendations />} />
-      <Route path="/recommendations" element={<EmotionRecommendations />} />
-      <Route path="/trending" element={<Trending />} />
+      <Route path="/index" element={<Index />} />
     </Routes>
   )
 }
